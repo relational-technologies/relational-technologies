0299aeadb8435d6639443e5d58806b2b0728168b
RTL <99806239+relational-technologies@users.noreply.github.com>
2022-07-18 10:29:14 +0100

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+		'name': 'Odoo Theme Backend',
+        'version' : '1.1',
+        'author': "Oluseyi ADEYEMI.",
+        'category': 'Odoo Theme Backend',
+        'website': 'https://adexbola.wixsite.com/copy-of-adexbola',
+		'summary': 'Odoo Theme Backend',
+		'sequence': 15,
+		'description': "Odoo Theme Backend",
+        'images' : ['images/journal_entries.jpeg'],
+		'depends': ['web'],
+		'data': [
+			'views/assets.xml',
+		],
+		'author': 'Oluseyi ADEYEMI',
+		'maintainer': 'Oluseyi ADEYEMI',
+		'support': 'adexbolabizzy@gmail.com',
+        'installable': True,
+        'application': True,
+        'auto_install': False,
+        'license': 'OEEL-1',
+	}
